a93cefbeb523ab111e473f21f5242c9cb121f4d7
MuteSTEVE <Dr.MuteSTEVE2@protonmailcom>
2023-12-12 21:12:41 +0700

    Initializing keymaps, options and Lazy

---
+++ lua/core/icons.lua
@@ -0,0 +1,93 @@
+return {
+  telescope = {
+    select = "",
+    find = " 󰍉"
+  },
+
+  nvimtree = {
+    GlyphDefault = "",
+    GlyphSymlink = "",
+    default = "󰉋",
+    open = "",
+    empty = "",
+    empty_open = "",
+    symlink = "",
+  },
+
+  git = {
+    unstaged = "",
+    staged = "S",
+    unmerged = "",
+    renamed = "➜",
+    deleted = "",
+    untracked = "U",
+    ignored = "◌",
+    signs = "▌"
+  },
+
+  kind_icons = {
+    Text = "󰉿",
+    Method = "",
+    Function = "",
+    Constructor = "",
+    Field = "",
+    Variable = "󰀫",
+    Class = "󰠱",
+    Interface = "",
+    Module = "",
+    Property = "",
+    Unit = "",
+    Value = "",
+    Enum = "",
+    Keyword = "󰌆",
+    Snippet = "󰆐",
+    Color = "󰏘",
+    File = "",
+    Reference = "",
+    Folder = "󰉋",
+    EnumMember = "",
+    Constant = "󰏿",
+    Struct = "",
+    Event = "",
+    Operator = "󰆕",
+    TypeParameter = " ",
+    Misc = "",
+  },
+
+  signs = {
+    Error = "󰅙 ",
+    Warn = " ",
+    Hint = "󰌵",
+    Info = " ",
+  },
+
+  lspsaga = {
+    error_sign = "",
+    warn_sign = "",
+    hint_sign = "󰌵",
+    infor_sign = "",
+    diagnostic_header_icon = "   ",
+    finder_definition_icon = "  ",
+    finder_reference_icon = "  ",
+    definition_preview_icon = "  ",
+    rename_prompt_prefix = "➤",
+  },
+
+  mason = {
+    package_installed = "●",
+    package_pending = "",
+    package_uninstalled = "○",
+  },
+
+  lualine = {
+    __1 = "__",
+    __2 = "▁▁",
+    __3 = "▂▂",
+    __4 = "▃▃",
+    __5 = "▄▄",
+    __6 = "▅▅",
+    __7 = "▆▆",
+    __8 = "▇▇",
+    __9 = "██"
+  }
+}
---
+++ lua/core/keymaps.lua
@@ -0,0 +1,48 @@
+-- Map Leader key
+vim.g.mapleader = ' '
+
+-- Define map as keybind
+local map = vim.api.nvim_set_keymap
+local optf = { noremap = true, silent = false }
+local opt = { noremap = true, silent = true }
+
+map("n", "<S-l>", "<cmd>Lazy<cr>", {})
+
+-- Some useful keybinds VSCode move line keybind
+map('n', '<Left>', '<C-w>h', optf)
+map('n', '<Down>', '<C-w>j', opt)
+map('n', '<Up>', '<C-w>k', opt)
+map('n', '<Right>', '<C-w>l', opt)
+
+map('n', 'K', "<cmd>move .-2<CR>==", opt)
+map('n', 'J', "<cmd>move .+1<CR>==", opt)
+map('v', 'K', "<cmd>move '<-2<CR>gv-gv", opt)
+map('v', 'J', "<cmd>move '>+1<CR>gv-gv", opt)
+
+-- buffer movement
+map('n', 'Z', '<cmd>bp<CR>', opt)
+map('n', 'C', '<cmd>bn<CR>', opt)
+map('n', 'X', '<cmd>bd<CR>', opt)
+
+-- Move between panes quickly
+map('n', '<C-h>', '<C-w>h', optf)
+map('n', '<C-l>', '<C-w>l', optf)
+map('n', '<C-j>', '<C-w>j', optf)
+map('n', '<C-k>', '<C-w>k', optf)
+
+-- Get out of insert mode quickly
+map('i', '<C-k>', '<ESC>', optf)
+
+-- Indent
+map('v', '<', '<gv', optf)
+map('v', '>', '>gv', optf)
+
+-- Automatically close brackets, parethesis, and quotes
+map("i", "'", "''<left>", opt)
+map("i", "\"", "\"\"<left>", opt)
+map("i", "(", "()<left>", opt)
+map("i", "[", "[]<left>", opt)
+map("i", "{", "{}<left>", opt)
+map("i", "{;", "{};<left><left>", opt)
+map("i", "/*", "/**/<left><left>", opt)
+
