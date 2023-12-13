e23c92855e39dde34246425c31d56d73f950c185
MuteSTEVE <Dr.MuteSTEVE2@protonmailcom>
2023-12-13 19:58:10 +0700

    "Colorizer" lazyload

---
+++ lua/plug/colorizer.lua
@@ -0,0 +1,18 @@
+local PLUG = {
+  'norcalli/nvim-colorizer.lua',
+  event = { "BufReadPost", "BufNewFile" },
+  lazy = true,
+  cmd = "Colorizer",
+  ft = { "css", "html", "php", "js", },
+}
+
+function PLUG.config()
+  local colorizer_ok, colorizer = pcall(require, "colorizer")
+  if not colorizer_ok then
+    return
+  end
+
+  colorizer.setup()
+end
+
+return PLUG
